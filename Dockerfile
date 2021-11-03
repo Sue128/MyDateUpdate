dd298077b18d7b6674eb19bdd9fdf5b709302508
Sue128 <kangsue128@gmail.com>
2021-11-03 07:07:38 +0000

    Wed Nov  3 07:07:38 UTC 2021

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,14 @@ MAINTAINER sue
 RUN mkdir MyDateUpdate
 COPY . ./MyDateUpdate
 #Install git and openssh
+RUN yum -y install vim
 RUN yum -y install git
 RUN yum -y install openssh-server
+
+#Specified WorkDir
+WORKDIR /MyDateUpdate
+#Run Command
+RUN echo `pwd`
+#CMD ["/MyDateUpdate/push.sh"]
+
+
--- push.sh
+++ push.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+#ssh agent add
+eval `ssh-agent`
+ssh-add ../.ssh/id_rsa
 
-#git config --global user.email "kangsue128@gmail.com"
-#git config --global user.name "Sue128"
-
+#who you are
+git config --global user.email "kangsue128@gmail.com"
+git config --global user.name "Sue128"
+#git push
 git add .
 echo "add ok!"
 git commit -m "$(date)"
 echo "commit ok!"
-git push origin main
+GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/.ssh/know_hosts -o StrictHostKeyChecking=no" git push origin main
 echo "push ok!"
