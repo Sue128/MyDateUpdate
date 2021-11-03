df153f79277d742d6a4f5473b23a228f6c449f08
Sue128 <kangsue128@gmail.com>
2021-11-03 19:51:26 +0900

    sb

--- push.sh
+++ push.sh
@@ -6,6 +6,8 @@ ssh-add ../.ssh/id_rsa
 #who you are
 git config --global user.email "kangsue128@gmail.com"
 git config --global user.name "Sue128"
+#push time
+echo "Upadet Date/Time:" $(date) >> homework1030.txt
 #git push
 git add .
 echo "add ok!"
@@ -13,5 +15,3 @@ git commit -m "$(date)"
 echo "commit ok!"
 GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/.ssh/know_hosts -o StrictHostKeyChecking=no" git push origin main
 echo "push ok!"
-
-echo "Upadet Date/Time:" $(date) >> homework1030.txt
